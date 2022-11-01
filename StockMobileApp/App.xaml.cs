4a8285f80afdddba19fe6fc73e7e6c842f96c279
Aung Myat Kyaw <aungmyatkyaw.kk@gmail.com>
2022-11-01 21:21:41 +0630

    Add project files.

---
+++ StockMobileApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace StockMobileApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ StockMobileApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace StockMobileApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
